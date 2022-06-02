4634625d5697b15bc96afb529baed3bedbedfaf2
badrogger <ivan@skalelabs.com>
2022-06-02 17:50:06 +0000

    SKALE-4904 Decorate wallet cli cmd with transaction_cmd

--- cli/wallet.py
+++ cli/wallet.py
@@ -1,8 +1,13 @@
 import logging
 import click
 
-from core.wallet import send_eth, send_skl, setup_ledger, wallet_info
-from utils.helper import abort_if_false
+from core.wallet import (
+    transfer_eth,
+    transfer_skl,
+    setup_ledger,
+    wallet_info
+)
+from utils.helper import abort_if_false, transaction_cmd
 from utils.texts import Texts
 from utils.constants import LEDGER_KEYS_TYPES
 
@@ -23,29 +28,22 @@ def wallet():
 
 
 @wallet.command('send-eth', help=TEXTS['send_eth']['help'])
+@transaction_cmd
 @click.argument('receiver_address')
 @click.argument('amount')
-@click.option(
-    '--pk-file',
-    help=G_TEXTS['pk_file']['help']
-)
 @click.option('--yes', is_flag=True, callback=abort_if_false,
               expose_value=False, prompt=TEXTS['send_eth']['confirm'])
-def _send_eth(receiver_address, amount, pk_file):
-    send_eth(receiver_address, amount, pk_file)
+def _send_eth(receiver_address, amount, pk_file, fee):
+    transfer_eth(receiver_address, amount, pk_file, fee=fee)
 
 
 @wallet.command('send-skl', help=TEXTS['send_skl']['help'])
 @click.argument('receiver_address')
 @click.argument('amount')
-@click.option(
-    '--pk-file',
-    help=G_TEXTS['pk_file']['help']
-)
 @click.option('--yes', is_flag=True, callback=abort_if_false,
               expose_value=False, prompt=TEXTS['send_skl']['confirm'])
-def _send_skl(receiver_address, amount, pk_file):
-    send_skl(receiver_address, amount, pk_file)
+def _send_skl(receiver_address, amount, pk_file, fee):
+    transfer_skl(receiver_address, amount, pk_file, fee=fee)
 
 
 @wallet.command('info', help=TEXTS['info']['help'])
