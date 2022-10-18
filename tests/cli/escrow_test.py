dda7dd1e74911b8946bd2c1ee68cd4d215499174
badrogger <ivan@skalelabs.com>
2022-10-18 19:09:26 +0000

    SKALE-4904 Remove debug info

--- tests/cli/escrow_test.py
+++ tests/cli/escrow_test.py
@@ -54,9 +54,6 @@ def _generate_and_terminate(skale_allocator_vm):
 
 
 def test_info(runner, skale_allocator_beneficiary):
-    from core.escrow import info as core_info
-    out = core_info(skale_allocator_beneficiary.wallet.address, wei=True)
-    print(out)
     result = runner.invoke(
         _info,
         [
